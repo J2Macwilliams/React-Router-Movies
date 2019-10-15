b251ba617eab58a3daf2ac60163d87260b0ea71b
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-15 16:35:18 -0700

    Add refactored MovieCard to MovieList.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 import axios from 'axios';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
+
   useEffect(() => {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies')
         .then(response => {
+          console.log(response);
           setMovies(response.data);
         })
         .catch(error => {
@@ -28,24 +32,12 @@ const MovieList = props => {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  // const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
+    <Link to={`/movies/${movie.id}`}>
+      <MovieCard movie={movie}/>
+    
+    </Link>
   );
 }
 
