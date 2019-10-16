0e3ad0edf52651c81d25add18a4e728d3f7b6dbc
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-16 12:16:28 -0700

    remove unecessary coments.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -23,10 +23,10 @@ const Movie = (props) => {
   },[props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -35,9 +35,8 @@ const Movie = (props) => {
   
   return (
     <div className="save-wrapper">
-      <MovieCard movie={movie}/>
-   
-      <div className="save-button">Save</div>
+      <MovieCard movie={movie} saveMovie={saveMovie}/>
+      <div onClick={saveMovie} className="save-button">Save</div>
     </div>
   );
 }
